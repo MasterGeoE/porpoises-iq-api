ffc4921d67aea166ce21e3806cab2205663c434e
DemonLord <120779070+NimithSan@users.noreply.github.com>
2023-10-26 18:42:42 +0700

    change provider

--- routes/api.py
+++ routes/api.py
@@ -2,7 +2,7 @@ from typing import List
 from fastapi import APIRouter
 from model.Message import Message
 import g4f
-from g4f.Provider import GptGo
+from g4f.Provider import GPTalk
 from deep_translator import GoogleTranslator
 
 router = APIRouter(tags=['API'])
@@ -35,7 +35,7 @@ def chat_v1_en(message_list: List[Message]):
         response = g4f.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=messages,
-            provider=GptGo,
+            provider=GPTalk,
         )
         return {"role":"assistant","content":response}
 
@@ -58,7 +58,7 @@ def chat_v1_km(message_list: List[Message]):
         response = g4f.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=messages,
-            provider=GptGo,
+            provider=GPTalk,
         )
         content = translator(content=response,target='km')
         return {"role":"assistant","content":content}
