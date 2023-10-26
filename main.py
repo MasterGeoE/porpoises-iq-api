3fa20c5464bbf2b4a1780e73d37175851c6940b4
DemonLord <120779070+NimithSan@users.noreply.github.com>
2023-10-26 11:24:31 +0700

    remove unuse

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from typing import List
 from fastapi import FastAPI, Request
-from g4f.Provider import (GptGo)
-import g4f
-from sdxl import ImageGenerator
 import uvicorn
-from model.Message import Message
 from fastapi.templating import Jinja2Templates
 from routes import api
 
