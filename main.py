fe82d9ded646bac6c8c7f702eda4cf29fad746dc
DemonLord <120779070+NimithSan@users.noreply.github.com>
2023-10-26 11:36:42 +0700

    ~

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
-from typing import List
-from fastapi import FastAPI, Request
-import uvicorn
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI
 from routes import api
 
 app = FastAPI()
 
 app.include_router(api.router)
 
-
-templates = Jinja2Templates(directory="templates") 
-
 @app.get('/')
-def root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-if __name__ == "__main__":
-    uvicorn.run(app, host= "0.0.0.0", port=80)
+def root():
+    return {"return":"error"}
