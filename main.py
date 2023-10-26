7622e5d17049d4592308a8be9f2f4283e86380b0
DemonLord <120779070+NimithSan@users.noreply.github.com>
2023-10-26 22:40:51 +0700

    add vercel file

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from routes import api
+from fastapi.templating import Jinja2Templates
+
 
 app = FastAPI()
 
 app.include_router(api.router)
 
-@app.get('/')
-def root():
-    return {"return":"error"}
+templates = Jinja2Templates(directory="templates") 
+
+@app.get("/")
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
